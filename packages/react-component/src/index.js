073b677ce6a6f44090b35f1deec9b08707ac294c
nate-fleming <nfleming4@gmail.com>
2019-10-09 15:09:42 -0500

    setup

---
+++ packages/react-component/src/index.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styled from "styled-components";
+
+const Title = styled.h1`
+  font-size: 1.5em;
+  text-align: center;
+  color: palevioletred;
+`;
+
+const Wrapper = styled.section`
+  padding: 4em;
+  background: papayawhip;
+`;
+
+function testComponent() {
+  return (
+    <Wrapper>
+      <Title>Hello World! from Github Package Registry</Title>
+    </Wrapper>
+  );
+}
+
+export default testComponent;
